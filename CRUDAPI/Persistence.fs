272e1d30c7a30ab763c90b095097e23516701666
nikolas note <nikolas note@DESKTOP-3ELJ89S>
2016-12-17 13:12:38 -0200

    Initial commit

---
+++ CRUDAPI/Persistence.fs
@@ -0,0 +1,46 @@
+﻿module Persistence
+
+open FSharp.Data.Sql
+open System
+
+let [<Literal>] private resolutionPath = "..\\..\\..\\packages\\MySql.Data.6.9.9\\lib\\net45\\MySql.Data.dll"
+let [<Literal>] connectionString = "Server=localhost;Database=controledecustos;Uid=root;Pwd=123456;"
+
+// create a type alias with the connection string and database vendor settings
+type sql = SqlDataProvider< 
+              ConnectionString = connectionString,
+              DatabaseVendor = Common.DatabaseProviderTypes.MYSQL,
+              ResolutionPath = resolutionPath,
+              IndividualsAmount = 1000,
+              UseOptionTypes = true >
+let ctx = sql.GetDataContext()
+
+
+let insertCentroDeCusto nome codigo =
+    let objeto = ctx.Controledecustos.Centrodecusto.Create(codigo, nome)
+    objeto.Id <- Guid.NewGuid().ToString()
+    ctx.SubmitUpdates()
+
+let insertLancamento (centroId:Guid) valor descricao =
+    let objeto = ctx.Controledecustos.Lancamento.Create(centroId.ToString(), DateTime.Now, descricao, valor)
+    objeto.Id <- Guid.NewGuid().ToString()
+    ctx.SubmitUpdates()
+
+let getCentroDeCusto (centroId:Guid) =
+    ctx.Controledecustos.Centrodecusto |> Seq.find (fun cc -> cc.Id = centroId.ToString())
+
+let getLancamento (lancamentoId:Guid) = 
+    ctx.Controledecustos.Lancamento |> Seq.find (fun l -> l.Id = lancamentoId.ToString())
+
+let getLancamentosPorCentroDeCusto (centroId:Guid)  = 
+    ctx.Controledecustos.Lancamento |> Seq.filter(fun l -> l.CentroDeCustoId = centroId.ToString())
+
+let deleteLancamento (lancamentoId:Guid) =
+    let objeto = getLancamento lancamentoId
+    objeto.Delete()
+    ctx.SubmitUpdates()
+
+let updateLancamento (lancamentoId:Guid) valor =
+    let objeto = getLancamento lancamentoId
+    objeto.Valor <- valor
+    ctx.SubmitUpdates()
---
+++ CRUDAPI/Server.fs
@@ -0,0 +1,28 @@
+﻿module Server
+
+open Suave
+open Suave.Successful
+open Suave.Filters
+open Suave.Operators
+open Suave.Http.HttpRequest
+open System.Web     
+open Newtonsoft
+open System.Text
+open System
+
+[<CLIMutable>]
+type lancamento = {CentroId:Guid; Valor:decimal; Descricao:string}
+
+let parseLanc input : lancamento =
+    Json.JsonConvert.DeserializeObject<lancamento>(input)
+
+let ``open`` () =
+    startWebServer defaultConfig (choose [ pathScan "/insert/centroDeCusto/%s/%s" (fun (nome, codigo) -> let q = HttpUtility.UrlDecode(nome)
+                                                                                                         let a = HttpUtility.UrlDecode(codigo)
+                                                                                                         Persistence.insertCentroDeCusto q a
+                                                                                                         OK (nome  + "_" + codigo))
+
+                                           path "/insert/lancamento" >=> POST >=> request(fun r -> let stringContent = Encoding.UTF8.GetString(r.rawForm)
+                                                                                                   let lanc = parseLanc stringContent
+                                                                                                   Persistence.insertLancamento lanc.CentroId lanc.Valor lanc.Descricao
+                                                                                                   OK "teste" ) ])
